d4ecbade33740538a16f2836961526eaaf34581f
Vinicius Rocha <viniciusr.rocha@outlook.com>
2020-01-30 10:35:31 -0300

    Adicionando FlatList

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text, ImageBackground, StyleSheet } from 'react-native'
+import { View, Text, ImageBackground, StyleSheet, FlatList } from 'react-native'
 
 import moment from 'moment'
 import 'moment/locale/pt-br'
@@ -9,6 +9,20 @@ import todayImage from '../../assets/imgs/today.jpg'
 import Task from '../components/Task'
 
 export default class TaskList extends Component {
+	state = {
+		tasks: [{
+			id: Math.random(),
+			desc: 'Estudar Certificação Java',
+			estimateAt: new Date(),
+			doneAt: new Date(),
+		}, {
+			id: Math.random(),
+			desc: 'Ler livro',
+			estimateAt: new Date(),
+			doneAt: null,
+		}]
+	}
+
     render() {
 
 		const today = moment().locale('pt-br').format('ddd, D [de] MMMM')
@@ -23,10 +37,9 @@ export default class TaskList extends Component {
 					</View>				
                 </ImageBackground>
 				<View style={styles.taskList}>
-					<Task desc="Estudar certificação Java" estimateAt={new Date()}
-						doneAt={new Date()} />
-					<Task desc="Ler livro" estimateAt={new Date()}
-						doneAt={null} />
+					<FlatList data={this.state.tasks}
+						keyExtractor={item => `${item.id}`} 
+						renderItem={({item}) => <Task {...item}/> } />
 				</View>
             </View>
         )
